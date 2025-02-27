2d889d2800e9953af4b863fbfd11abc2334412ba
Ammar Mahmoud <ammarma.eid@gmail.com>
2025-02-27 12:30:37 +0200

    Main.java

--- Students-Grades-Tracker/Main.java
+++ Students-Grades-Tracker/Main.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         System.out.println("To Enter the grades manually Enter 1 \nTo use existing file enter 2\nTo End Program Enter 9");
@@ -16,21 +14,17 @@ public class Main {
             case 2:
                 Read.readFromFile();
                 break;
+            case 9:
+                break;
         }
-
-        do{
-            System.out.println("Choose:\n1: Average.\n2: Highest\n3: Lowest\n4: Exit");
+        while (choice!=9){
+            System.out.println("Choose:\n1: Average.\n2: Highest\n3: Lowest\n9: Exit");
             choice = sc.nextInt();
             if ( choice==1 ) System.out.println(Calc.Average());
             else if ( choice==2 )System.out.println(Calc.Highest());
             else if ( choice==3 )System.out.println(Calc.Lowest());
 
-        }while (choice!=4);
-
-
-
-
-
+        }
+        System.out.println("End Program");
     }
-
 }
